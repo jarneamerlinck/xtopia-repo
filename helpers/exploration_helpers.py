f9fe667ecf09d956bd8214172a4bc980aebccf97
Jarne Amerlinck <r0817494@student.vives.be>
2022-10-13 17:16:43 +0200

    load model added

--- helpers/exploration_helpers.py
+++ helpers/exploration_helpers.py
@@ -8,12 +8,14 @@ def create_waveplot(data, sr, e):
     plt.figure(figsize=(10, 3))
     plt.title('Waveplot for audio with {} emotion'.format(e), size=15)
     librosa.display.waveshow(data, sr=sr)
-    plt.show()
+    return plt.show()
 
 def create_spectrogram(data, sr, e):
+    
+    fig = plt.figure(figsize=(12, 3))
     X = librosa.stft(data)
     Xdb = librosa.amplitude_to_db(abs(X))
-    plt.figure(figsize=(12, 3))
+    
     plt.title('Spectrogram for audio with {} emotion'.format(e), size=15)
     librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')   
-    plt.colorbar()
+    return plt.colorbar()
